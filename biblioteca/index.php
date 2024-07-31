565cdaaf932851dcdb66a2c0b652d6f6f3860029
mw-bl <bezerram533@gmail.com>
2024-07-31 14:23:30 -0300

    Add index.php

--- biblioteca/index.php
+++ biblioteca/index.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Cadastro de Livro</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <title>Cadastro de Livro</title>
+    </head>
+    <body>
+        <div>
+            <h1>Cadastro de Livro</h1>
+
+            <form action="" method="post">
+
+                <label for="titulo">Titulo</label>
+                <input type="text" id="titulo" required>
+                
+                <label for="autor">Autor</label>
+                <input type="text" id="autor" required>
+                
+                <label for="ano">Ano</label>
+                <input type="number" id="ano" required>
+
+                <input type="submit" value="Cadastrar">
+
+            </form>
+        </div>
+    </body>
 </html>
