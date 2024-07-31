063ec84c945abf8d6917312df7b3576e7a68ac2e
mw-bl <bezerram533@gmail.com>
2024-07-31 13:57:29 -0300

    :boom: fix: removed unused functions

--- atv/Livro.php
+++ atv/Livro.php
@@ -11,28 +11,19 @@ class Livro {
         $this -> autor = $autor;
     }
 
-    // Getter/Setter Titulo
+    // Getter Titulo
     public function getTitulo() {
         return $this -> titulo;
     }
-    public function setTitulo($titulo) {
-        $this -> titulo = $titulo;
-    }
 
-    // Getter/Setter Ano
+    // Getter Ano
     public function getAno() {
         return $this -> ano;
     }
-    public function setAno($ano) {
-        $this -> ano = $ano;
-    }
 
-    // Getter/Setter Autor
+    // Getter Autor
     public function getAutor() {
         return $this -> autor;
-    }
-    public function setAutor($autor) {
-        $this -> autor = $autor;
     }
 }
 
