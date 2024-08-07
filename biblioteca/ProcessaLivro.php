7486f42e34fcbd9d4baead8a26bf9146b1cde057
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-08-07 14:49:02 -0300

    :tada: Commit Inicial

---
+++ biblioteca/ProcessaLivro.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+include_once 'Livro.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $ano = $_POST['ano'];
+
+    $livro = new Livro($titulo, $autor, $ano);
+
+    $_SESSION['livro'] = serialize($livro);
+
+    $mensagem = 'Livro cadastrado com sucesso!';
+}
+
+$livro_recuperado = isset($_SESSION['livro']) ? unserialize($_SESSION['livro']) : null;
+
+?>
